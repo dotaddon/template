407f5062974752f8dbf08c357dd6ab17be1a1e7d
西索酱 <405773654@qq.com>
2021-03-18 11:10:36 +0800

    【编译】批量编译工具进度条
    222

--- scripts/build.js
+++ scripts/build.js
@@ -2,11 +2,44 @@ const assert = require('assert');
 const fs = require('fs-extra');
 const path = require('path');
 const { spawn } = require('child_process');
-const { getAddonName, getDotaPath } = require('./utils');
+const { getAddonName, getDotaPath, read_all_files, ProgressBar } = require('./utils');
 
-// 不需要编译的规则
-const needResource = (file)=>{
-    return false
+/**
+ * 不需要被编译的过滤
+ */
+const buildFilter = (file)=>{
+    return !fs.existsSync(file)
+    || file.indexOf('panorama/src')>0
+    || file.indexOf('config')>0
+}
+
+/* 这里是模拟 DB 的单条写入更新的异步任务 */
+function update() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+        resolve();
+        }, 1000);
+    });
+}
+
+/* 单次任务，并行执行3条 */
+async function singleTask(arr, win64, targetRoot) {
+await Promise.all(
+    arr.map(async (file) => {
+    try {
+        // console.log("正在处理", file);
+        const args = [ '-f', '-i', file, '-outroot', targetRoot ];
+        spawn(path.join(win64, 'resourcecompiler.exe'), args );
+        // 这个 update 方法就是你的 userModel.findOrCreate 方法，你需要自己替换一下
+        await update({
+        //   where: _.pick(info, ["taskId", "challengeId", "uid"]),
+        //   defaults: info,
+        });
+    } catch (error) {
+        // error
+    }
+    })
+);
 }
 
 (async () => {
@@ -19,17 +52,38 @@ const needResource = (file)=>{
     const sourcePath = path.join( sourceRoot , getAddonName());
     assert(fs.existsSync(sourcePath), `无法找到目录 '${sourcePath}' 请先执行 install`);
 
-    const targetPath = path.join( dotaPath, 'game');
-    assert(fs.existsSync(targetPath), `无法找到目录 '${targetPath}'`);
+    const targetRoot = path.join( dotaPath, 'game');
+    assert(fs.existsSync(targetRoot), `无法找到目录 '${targetRoot}'`);
 
     const files = read_all_files(sourcePath);
+    const MAX_COUNT = 3;
 
-    files.forEach((file) => {
-        if( needResource(file) ) return;
+    const userTasks = files
+    .filter(ele=>!buildFilter(ele))
+    .reduce(
+      (acc, v) => {
+        const lastArr = acc[acc.length - 1];
+        if (lastArr.length < MAX_COUNT) {
+          lastArr.push(v);
+        } else {
+          acc.push([v]);
+        }
+        return acc;
+      },
+      [[]]
+    );
 
-        const args = ['-i', file, '-outroot', targetPath ];
-        spawn(path.join(win64, 'resourcecompiler.exe'), args );
-    });
+    // 初始化一个进度条长度为 25 的 ProgressBar 实例
+    var pb = new ProgressBar('编译进度',25);
+    let index = 1;
+    for (const arr of userTasks) {
+        // 更新进度条
+        pb.render({ completed: index, total: userTasks.length });
+        // console.log(`开始处理第${index}组:`);
+        await singleTask(arr, win64, targetRoot);
+        // console.log(`第${index}组:处理完毕`);
+        index++;
+    }
 
 })().catch((error) => {
     console.error(error);
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,7 @@
 const { findSteamAppByName, SteamNotFoundError } = require('find-steam-app');
 const packageJson = require('../package.json');
 const fs = require('fs-extra');
+const slog = require('single-line-log').stdout;//用以在同一行打印文本
 
 module.exports.getAddonName = () => {
     if (!/^[a-z]([\d_a-z]+)?$/.test(packageJson.name)) {
@@ -59,3 +60,37 @@ read_sub_directories = (path) => {
     return directories;
 };
 module.exports.read_sub_directories = read_sub_directories;
+
+
+// 封装的 ProgressBar 工具
+function ProgressBar(description, bar_length){
+    // 两个基本参数(属性)
+    this.description = description || 'Progress';    // 命令行开头的文字信息
+    this.length = bar_length || 25;           // 进度条的长度(单位：字符)，默认设为 25
+
+    // 刷新进度条图案、文字的方法
+    this.render = function (opts){
+    var percent = (opts.completed / opts.total).toFixed(4);  // 计算进度(子任务的 完成数 除以 总数)
+    var cell_num = Math.floor(percent * this.length);       // 计算需要多少个 █ 符号来拼凑图案
+
+    // 拼接黑色条
+    var cell = '';
+    for (var i=0;i<cell_num;i++) {
+        cell += '█';
+    }
+
+    // 拼接灰色条
+    var empty = '';
+    for (var i=0;i<this.length-cell_num;i++) {
+        empty += '░';
+    }
+
+    // 拼接最终文本
+    var cmdText = this.description + ': ' + (100*percent).toFixed(2) + '% ' + cell + empty + ' ' + opts.completed + '/' + opts.total;
+
+    // 在单行输出文本
+    slog(cmdText);
+    };
+}
+
+module.exports.ProgressBar = ProgressBar;
