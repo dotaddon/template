3f722b2b2c848f1e423c86b8507067c8c89c994f
西索酱 <405773654@qq.com>
2021-08-11 23:03:58 +0800

    导入商店编辑模块

--- toolCode/install.js
+++ toolCode/install.js
@@ -20,11 +20,15 @@ const items = {
     信息:'addoninfo.txt',
     事件:'scripts/custom.gameevents',
     网表:'scripts/custom_net_tables.txt',
-    单位:'scripts/npc/npc_units_custom.txt',
-    技能:'scripts/npc/npc_abilities_custom.txt',
-    英雄:'scripts/npc/npc_heroes_custom.txt',
-    物品:'scripts/npc/npc_items_custom.txt',
+    人像:'scripts/npc/portraits_custom.txt',
     激活:'scripts/npc/herolist.txt',
+    英雄:'scripts/npc/npc_heroes_custom.txt',
+    单位:'scripts/npc/npc_units_custom.txt',
+    物品:'scripts/npc/npc_items_custom.txt',
+    中立:'scripts/npc/npc_neutral_items_custom.txt',
+    技能:'scripts/npc/npc_abilities_custom.txt',
+    覆盖:'scripts/npc/npc_abilities_override.txt',
+    商店:'scripts/shops/main_shops.txt',
 }
 
 function linkA2B(sourcePath,targetPath) {
