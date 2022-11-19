137a2e120fa7e539e26b96c952f68335f31296e8
西索酱 <takegine@vip.qq.com>
2022-11-19 12:41:52 +0800

    尝试混合rollup

--- .bin/esbuild.ts
+++ .bin/esbuild.ts
@@ -1,8 +1,8 @@
 import { build } from 'esbuild'
 import { pack,CreateXml } from '@mobilc/utils';
 import { xml } from '../layout.config.json';
-const { watch } = pack(xml as any);
-
+const { watch,compiler } = pack(xml as any);
+const bDev = process.argv.includes('--watch')
 CreateXml(xml as any)
 build({
     ...watch(),
