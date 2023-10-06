f1d19c9ca37d196a528cb7d6fffc81773b36b654
西索酱 <takegine@vip.qq.com>
2023-10-06 12:35:08 +0800

    更新依赖

--- .bin/image.ts
+++ .bin/image.ts
@@ -1,4 +1,4 @@
 import { assets } from '@dotaddon/packer'
 import layout from '../程序/panorama/layout.config.json';
 
-assets(layout['image-loader'], 'images_precache.xml')
+assets(layout['image-loader'], 98, 'images_precache.xml')
--- .bin/nettable.ts
+++ .bin/nettable.ts
@@ -3,12 +3,13 @@ import { getAddonPath, listen } from '@dotaddon/rigger';
 import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
+const filePath = '程序/shared/nettable/dota_net_table_keys.json'
 const { server } = getAddonPath()
-listen(['程序/shared/nettable/dota_net_table_keys.json'],async file=>{
+listen([filePath],async file=>{
     
     let w = `<!-- kv3 encoding:text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d} format:generic:version{7412167c-06e9-4698-aff2-e63eb59037e7} -->
 {
-    custom_net_tables = ${readFileSync('程序/shared/nettable/dota_net_table_keys.json')}
+    custom_net_tables = ${readFileSync(filePath)}
 }`
     writeFileSync(join(server, 'scripts', 'custom_net_tables.txt'), w)
 })
